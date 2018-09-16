66c38ae6dbec9d0da2294e7ac7fb627ac5643b0b
Denys Gergun <gergund@gmail.com>
2018-09-16 18:35:22 +0300

    Fix

--- nginx/scripts/run.sh
+++ nginx/scripts/run.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
+
+set -e
+
+if [ -z $BASIC_AUTH_USERNAME ]; then
+  echo >&2 "BASIC_AUTH_USERNAME must be set"
+  exit 1
+fi
+
+if [ -z $BASIC_AUTH_PASSWORD ]; then
+  echo >&2 "BASIC_AUTH_PASSWORD must be set"
+  exit 1
+fi
+
 /usr/bin/htpasswd -bBc /etc/nginx/.htpasswd $BASIC_AUTH_USERNAME $BASIC_AUTH_PASSWORD
+
+exec nginx -g "daemon off;"
